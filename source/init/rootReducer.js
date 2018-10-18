78b9378a2c91b15589c6986a60f70027968759a3
Andrey Smirnov <iadisey@gmail.com>
2018-10-19 00:53:14 +0300

    Make reducer Tasks

--- source/init/rootReducer.js
+++ source/init/rootReducer.js
@@ -3,7 +3,9 @@ import { combineReducers } from 'redux';
 
 // Reducers
 import { authenticateReducer as authenticate } from '../bus/authenticate/reducer';
+import { tasksReducer as tasks } from '../bus/tasks/reducer';
 
 export const rootReducer = combineReducers({
     authenticate,
+    tasks,
 });
--- source/init/rootSaga.js
+++ source/init/rootSaga.js
@@ -3,7 +3,9 @@ import { all, call } from 'redux-saga/effects';
 
 // Wathers
 import { watchAuthentication } from '../bus/authenticate/saga/watchers';
+import { watcherTasks } from '../bus/tasks/saga/watchers';
 
 export function* rootSaga () {
     yield all([ call(watchAuthentication) ]);
+    yield all([ call(watcherTasks) ]);
 }
