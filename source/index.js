f0bce167d40495b3951fdadb81b952ce3469fcc9
Andrey Smirnov <iadisey@gmail.com>
2018-10-18 12:34:35 +0300

    Start

---
+++ source/index.js
@@ -0,0 +1,10 @@
+// Core
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Theme
+import './theme/init';
+
+const start = <div className = 'start'>Start!</div>;
+
+ReactDOM.render(start, document.getElementById('app'));
