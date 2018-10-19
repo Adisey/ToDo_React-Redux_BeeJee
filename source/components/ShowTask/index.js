14ac7e9a16807b4ce74198bbae0f9a47b2c645f2
Andrey Smirnov <iadisey@gmail.com>
2018-10-19 16:54:41 +0300

    Start Edit

--- source/components/ShowTask/index.js
+++ source/components/ShowTask/index.js
@@ -28,6 +28,19 @@ export default class ShowTask extends Component {
         const _text = tasks.getIn([ 'tempTask', 'text' ]);
         const _status = tasks.getIn([ 'tempTask', 'status' ]);
         const _image_path = tasks.getIn([ 'tempTask', 'image_path' ]);
+        const _iconStatus = _status ? (
+            <Icon
+                theme = 'twoTone'
+                twoToneColor = '#52c41a'
+                type = 'check-circle'
+            />
+        ) : (
+            <Icon
+                theme = 'twoTone'
+                twoToneColor = '#eb2f96'
+                type = 'clock-circle'
+            />
+        );
 
         return (
             <Modal
@@ -42,24 +55,23 @@ export default class ShowTask extends Component {
                         type = 'primary'>Закрыть
                     </Button>,
                 ] }>
-                <Card >
+                <Card
+                    cover = { _image_path
+                        ? <img
+                            alt = 'example'
+                            src = { _image_path }
+                          />
+                        : <Avatar
+                            icon = 'picture'
+                            shape = 'square'
+                            size = { 128 }
+                        /> }>
                     <Meta
-                        avatar = { _image_path
-                            ? <Avatar
-                                shape = 'square'
-                                size = { 128 }
-                                src = { _image_path }
-                              />
-                            : <Avatar
-                                icon = 'picture'
-                                shape = 'square'
-                                size = { 128 }
-                              /> }
                         description = <div className = { Styles.description }>
                             <p>Пользхователь: <span>{_username}</span></p>
                             <p>Email: <span>{_email}</span></p>
                                       </div>
-                        title = { _text }
+                        title = { <p>{_iconStatus} {_text}</p> }
                     />
                 </Card>
             </Modal>
--- source/components/TasksList/index.js
+++ source/components/TasksList/index.js
@@ -8,13 +8,73 @@
 // Core
 import React, { Component } from 'react';
 // Antd
-import { Button, Table, Icon, Input } from 'antd';
+import { Button, Table, Icon, Input, Popover, Form } from 'antd';
+const FormItem = Form.Item;
+const EditableContext = React.createContext();
 
 // Styles
 import cx from 'classnames';
 import Styles from './styles.m.css';
 // Components
 // Actions
+
+//------------Edit---------------------------------------------
+
+const EditableRow = ({ form, index, ...props }) => (
+    <EditableContext.Provider value={form}>
+        <tr {...props} />
+    </EditableContext.Provider>
+);
+
+const EditableFormRow = Form.create()(EditableRow);
+
+class EditableCell extends React.Component {
+    getInput = () => {
+        if (this.props.inputType === 'number') {
+            return <InputNumber />;
+        }
+        return <Input />;
+    };
+
+    render() {
+        const {
+            editing,
+            dataIndex,
+            title,
+            inputType,
+            record,
+            index,
+            ...restProps
+        } = this.props;
+        return (
+            <EditableContext.Consumer>
+                {(form) => {
+                    const { getFieldDecorator } = form;
+                    return (
+                        <td {...restProps}>
+                            {editing ? (
+                                <FormItem style={{ margin: 0 }}>
+                                    {getFieldDecorator(dataIndex, {
+                                        rules: [{
+                                            required: true,
+                                            message: `Please Input ${title}!`,
+                                        }],
+                                        initialValue: record[dataIndex],
+                                    })(this.getInput())}
+                                </FormItem>
+                            ) : restProps.children}
+                        </td>
+                    );
+                }}
+            </EditableContext.Consumer>
+        );
+    }
+}
+
+
+
+//------------Edit---------------------------------------------
+
 
 
 export default class TasksList extends Component {
@@ -35,6 +95,7 @@ export default class TasksList extends Component {
                 title:     'Email',
                 dataIndex: 'email',
                 key:       'email',
+                editable:  true,
             },
             {
                 title:     'Задача',
@@ -69,13 +130,30 @@ export default class TasksList extends Component {
                 username: t.get('username'),
                 email:    t.get('email'),
                 text:     t.get('text'),
-                status:   t.get('status'),
-                image:    t.get('image_path')
-                    ? <img
-                        height = '24'
-                        src = { t.get('image_path') }
-                        width = '32'
-                      /> : '',
+                status:   t.get('status')
+                    ? <Icon
+                        theme = 'twoTone'
+                        twoToneColor = '#52c41a'
+                        type = 'check-circle'
+                    /> : <Icon
+                          theme = 'twoTone'
+                          twoToneColor = '#eb2f96'
+                          type = 'clock-circle'
+                           />,
+                image: t.get('image_path')
+                    ? <Popover
+                        content = { <img
+                            alt = { t.get('text') }
+                            height = '240'
+                            src = { t.get('image_path') }
+                        /> }
+                        title = { t.get('text') }
+                        trigger = 'hover'>
+                        <img
+                            height = '24'
+                            src = { t.get('image_path') }
+                        />
+                    </Popover> : '',
             };
         })
             .toJS();
